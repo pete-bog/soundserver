d350629cdb1ca40c315a076fbbd393c48954459f
Peter W <156520281+pete-bog@users.noreply.github.com>
2021-03-23 23:04:43 +0000

    Add from URL functionality

--- soundserver/main.py
+++ soundserver/main.py
@@ -1,9 +1,12 @@
 import functools
 import os
 import os.path
-from typing import Tuple
+import tempfile
 import urllib
+from typing import Tuple
+
 import bottle
+import requests
 
 from . import cli
 
@@ -11,19 +14,21 @@ STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
 PERMITTED_TYPES = ('.wav', '.mp3')
 
 
-def split_filename(filename: str) -> Tuple[str, str]:
-    if '.' not in filename:
-        return filename
-    rev_filename = list(reversed(filename))
-    final_dot_index = len(filename) - 1 - rev_filename.index('.')
-    return filename[:final_dot_index], filename[:final_dot_index]
-
-
 def replace_url_path(url: str, new_path: str) -> str:
     parsed = urllib.parse.urlparse(url)._asdict()
     parsed['path'] = new_path
     new_url = urllib.parse.ParseResult(**parsed)
     return urllib.parse.urlunparse(new_url)
+
+
+def check_file_extension(filename: str) -> str:
+    _, ext = os.path.splitext(filename)
+    if ext not in PERMITTED_TYPES:
+        raise bottle.HTTPError(
+            status=400,
+            body='File extension not allowed. Must be one of: {}.'.format(
+                ', '.join(PERMITTED_TYPES)))
+    return ext
 
 
 class SoundServer:
@@ -40,6 +45,7 @@ class SoundServer:
         # Stored non-application files
         self.app.route('/files/<filename:path>', 'GET', self.get_file)
         self.app.route('/files/upload', 'POST', self.upload_file)
+        self.app.route('/files/add-from-url', 'POST', self.add_from_url)
 
     def get_file_list(self):
         catalog = {'files': []}
@@ -57,13 +63,18 @@ class SoundServer:
 
     def upload_file(self):
         upload = bottle.request.files['upload']
-        _, ext = os.path.splitext(upload.filename)
-        if ext not in PERMITTED_TYPES:
-            return 'File extension not allowed. Must be one of: {}.'\
-                .format(', '.join(PERMITTED_TYPES))
-
+        check_file_extension(upload)
         upload.save(self.file_store)  # appends upload.filename automatically
-        return 'OK'
+        return 'ok'
+
+    def add_from_url(self):
+        ext = check_file_extension(bottle.request.forms["url"])
+        filename = os.path.join(self.file_store,
+                                bottle.request.forms["name"] + ext)
+        with open(filename, 'wb') as fp:
+            response = requests.get(bottle.request.forms["url"])
+            fp.write(response.content)
+        return 'ok'
 
     def run(self, *args, **kwargs):
         self.app.run(*args, **kwargs)
