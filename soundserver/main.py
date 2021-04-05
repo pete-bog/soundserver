80f877e9680691c3fb8a15880f37aad83e1d2929
Peter W <156520281+pete-bog@users.noreply.github.com>
2021-04-05 13:22:12 +0100

    Add download on request feature

--- soundserver/main.py
+++ soundserver/main.py
@@ -1,24 +1,21 @@
 import functools
 import os
 import os.path
-import tempfile
-import urllib
-from typing import Tuple
-
+import json
+import re
+import logging
+from datetime import datetime, timedelta
+from typing import Dict, Tuple
 import bottle
 import requests
 
-from . import cli
+from . import cli, utils
 
+LOG = logging.getLogger(__name__)
 STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
+THIRD_PARTY_DIR = os.path.join(os.path.dirname(__file__), "thirdparty")
 PERMITTED_TYPES = ('.wav', '.mp3')
-
-
-def replace_url_path(url: str, new_path: str) -> str:
-    parsed = urllib.parse.urlparse(url)._asdict()
-    parsed['path'] = new_path
-    new_url = urllib.parse.ParseResult(**parsed)
-    return urllib.parse.urlunparse(new_url)
+PERMITTED_URL_CHARS = re.compile(r'[A-Za-z0-9\-_~]')
 
 
 def check_file_extension(filename: str) -> str:
@@ -31,7 +28,31 @@ def check_file_extension(filename: str) -> str:
     return ext
 
 
+def make_sound_name(url, name=None) -> Tuple[str, str]:
+    if name is None:
+        name = utils.extract_filename_from_url(url)
+    url_safe_name = utils.make_url_safe_str(name)
+    ext = utils.get_file_ext(url)
+    # Return the extensionless name & the name with extension
+    # eg. my-sound, my-sound.mp3
+    short_name = url_safe_name
+    if short_name.endswith(ext):
+        short_name = short_name[:-len(ext)]
+    return short_name, short_name + ext
+
+
+def download_remote_file(url, save_as):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_as, "wb") as fp:
+            fp.write(response.content)
+    else:
+        raise bottle.HTTPError(
+            500, f"There was an error getting the url {url}: {response.text}")
+
+
 class SoundServer:
+    MAP_INTERVAL = timedelta(minutes=5)
 
     def __init__(self, file_store_dir):
         self.file_store = file_store_dir
@@ -47,18 +68,93 @@ class SoundServer:
         self.app.route('/files/upload', 'POST', self.upload_file)
         self.app.route('/files/add-from-url', 'POST', self.add_from_url)
 
-    def get_file_list(self):
-        catalog = {'files': []}
+        self.all_file_names = set()
+        self.all_files = list()
+        self.last_map_time = None
+
+    def build_file_maps(self):
+        self.all_files = []
+        self.all_file_names = set()
+        # Local files
         for filename in os.listdir(self.file_store):
-            url = replace_url_path(str(bottle.request.url),
-                                   f'/static/{filename}')
-            catalog['files'].append({
-                'name': os.path.splitext(filename)[0],
-                'url': url
-            })
-        return catalog
+            name, full_name = make_sound_name(filename)
+            LOG.debug("Adding local file '%s' to map", name)
+            self.all_files.append({'name': name, 'full_name': full_name})
+            self.all_file_names.add(full_name)
+
+        # Third party
+        for third_party_file in os.listdir(THIRD_PARTY_DIR):
+            with open(os.path.join(THIRD_PARTY_DIR, third_party_file)) as fp:
+                sounds = json.load(fp)
+            for entry in sounds:
+                name, full_name = make_sound_name(entry['url'],
+                                                  name=entry['name'])
+                if name in self.all_file_names:
+                    LOG.debug(
+                        "File named '%s' already exists in map, not adding "
+                        "to map", name)
+                else:
+                    self.all_files.append({
+                        'name': name,
+                        'full_name': full_name,
+                        'url': entry['url'],
+                    })
+                    self.all_file_names.add(full_name)
+        LOG.debug("Added %d sounds to map", len(self.all_files))
+
+        # Record when we build this map
+        self.last_map_time = datetime.utcnow()
+        LOG.debug('Successfully built map at %s', self.last_map_time)
+        return self.all_files
+
+    @property
+    def enriched_file_map(self) -> Dict:
+        """
+        Create a JSON-compatible list of files, with the urls patched to
+        the requester's hostname. Eg. if the client requested localhost, patch
+        all urls to localhost/files/file.wav
+        This fn also overrides any 'remote' urls with local ones that are
+        handled by get_file.
+        """
+        fmap = {'files': list(self.all_files)}
+        for i in range(len(fmap['files'])):
+            fmap['files'][i]['url'] = utils.replace_url_path(
+                str(bottle.request.url),
+                '/files/{}'.format(fmap['files'][i]['full_name']))
+        return fmap
+
+    def get_file_list(self):
+        # Return the cached map, if we built it recently
+        if self.last_map_time and \
+                datetime.utcnow() - self.last_map_time < self.MAP_INTERVAL:
+            return self.enriched_file_map
+
+        # Otherwise build the map now
+        self.build_file_maps()
+        # Return the enriched copy
+        return self.enriched_file_map
+
+    def get_data_for_filename(self, filename):
+        if filename in self.all_file_names:
+            for x in self.all_files:
+                if x['full_name'] == filename:
+                    return x
+        raise FileNotFoundError
 
     def get_file(self, filename):
+        try:
+            data = self.get_data_for_filename(filename)
+        except FileNotFoundError:
+            raise bottle.HTTPError(404, "File not found") from None
+
+        if 'url' in data:
+            # This is a remote file, so download & save it now
+            download_remote_file(
+                data['url'], os.path.join(self.file_store, data['full_name']))
+            # Rebuild the filemap
+            self.build_file_maps()
+
+        # Return the file that is guaranteed to exist in the file store
         return bottle.static_file(filename, root=self.file_store)
 
     def upload_file(self):
@@ -77,11 +173,14 @@ class SoundServer:
         return 'ok'
 
     def run(self, *args, **kwargs):
+        # Build initial map on start-up
+        self.build_file_maps()
         self.app.run(*args, **kwargs)
 
 
 def main():
     args = cli.parse_args()
+    LOG.setLevel(logging.DEBUG if args.dev_mode else logging.INFO)
     sound_server = SoundServer(args.store)
     sound_server.run(server='wsgiref' if args.dev_mode else 'tornado',
                      host=args.host,
---
+++ soundserver/utils.py
@@ -0,0 +1,49 @@
+"""Utility methods for soundserver"""
+import os
+import os.path
+
+import re
+import logging
+import urllib
+
+LOG = logging.getLogger(__name__)
+
+PERMITTED_URL_CHARS = re.compile(r'[A-Za-z0-9\-_~\.]')
+
+
+def replace_url_path(url: str, new_path: str) -> str:
+    """Swap the path section of a url (eg. localhost:8080)"""
+    parsed = urllib.parse.urlparse(url)._asdict()
+    parsed['path'] = new_path
+    new_url = urllib.parse.ParseResult(**parsed)
+    return urllib.parse.urlunparse(new_url)
+
+
+def extract_filename_from_url(url: str) -> str:
+    """Given a url to a static file, return just the filename
+    Eg. web.site/files/123.txt -> 123.txt
+    """
+    parsed = urllib.parse.urlparse(url)
+    return parsed.path.split("/")[-1]
+
+
+def make_url_safe_str(input_str: str) -> str:
+    """Make the supplied name URL safe by excluding/replacing characters"""
+    safe = ""
+    for char in input_str:
+        if char == " ":
+            # change spaces to dashes
+            safe += "-"
+        elif PERMITTED_URL_CHARS.match(char):
+            # only add permitted chars, ignore anything else
+            safe += char
+    return safe
+
+
+def remove_file_ext(filename: str) -> str:
+    name = os.path.splitext(filename)[0]
+    return name.rstrip(".")
+
+
+def get_file_ext(filename: str) -> str:
+    return os.path.splitext(filename)[1]
