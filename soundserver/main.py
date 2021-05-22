484c808302abf76b2467a0b32388112cd5d821b2
Peter W <156520281+pete-bog@users.noreply.github.com>
2021-05-22 11:56:09 +0100

    Conversion to sanic web app

--- soundserver/main.py
+++ soundserver/main.py
@@ -1,19 +1,19 @@
-import functools
 import json
-import logging
 import os
 import os.path
 import re
 from datetime import datetime, timedelta
 from typing import Dict, List, Tuple
 
-import bottle
+import aiofiles
+import sanic
 import fuzzywuzzy.process
-import requests
+import httpx
+
+from sanic.log import logger
 
 from . import cli, utils
 
-LOG = logging.getLogger(__name__)
 STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
 THIRD_PARTY_DIR = os.path.join(os.path.dirname(__file__), "thirdparty")
 PERMITTED_TYPES = ('.wav', '.mp3')
@@ -24,9 +24,8 @@ DEFAULT_SEARCH_LIMIT = 5
 def check_file_extension(filename: str) -> str:
     _, ext = os.path.splitext(filename)
     if ext not in PERMITTED_TYPES:
-        raise bottle.HTTPError(
-            status=400,
-            body='File extension not allowed. Must be one of: {}.'.format(
+        raise sanic.exceptions.InvalidUsage(
+            'File extension not allowed. Must be one of: {}.'.format(
                 ', '.join(PERMITTED_TYPES)))
     return ext
 
@@ -44,56 +43,67 @@ def make_sound_name(url, name=None) -> Tuple[str, str]:
     return short_name, short_name + ext
 
 
-def download_remote_file(url, save_as):
-    LOG.info("Downloading '%s' to '%s'", url, save_as)
-    response = requests.get(url)
+async def download_remote_file(url, save_as):
+    logger.info("Downloading '%s' to '%s'", url, save_as)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
     if response.status_code == 200:
-        with open(save_as, "wb") as fp:
-            fp.write(response.content)
+        async with aiofiles.open(save_as, "wb") as fp:
+            await fp.write(response.content)
     else:
-        raise bottle.HTTPError(
-            500, f"There was an error getting the url {url}: {response.text}")
+        raise sanic.exceptions.ServerError(
+            f"There was an error getting the url {url}: {response.text}")
 
 
 def find_closest_matches(search_term,
                          choices,
                          limit=5) -> List[Tuple[str, int]]:
-    LOG.info("Searching for %d similar matches to '%s'", limit, search_term)
+    logger.info("Searching for %d similar matches to '%s'", limit, search_term)
     matches = fuzzywuzzy.process.extract(search_term, choices, limit=limit)
-    LOG.info("Found matches: %s", matches)
+    logger.info("Found matches: %s", matches)
     return matches
+
+
+async def redirect_home(_):
+    return sanic.response.redirect("/static/index.html")
 
 
 class SoundServer:
     MAP_INTERVAL = timedelta(minutes=5)
 
-    def __init__(self, file_store_dir):
+    def __init__(self, file_store_dir: str):
         self.file_store = file_store_dir
-        self.app = bottle.Bottle()
-        self.app.route('/', 'GET',
-                       functools.partial(bottle.redirect, "static/index.html"))
-        # Static application files (html, css, js, ...)
-        self.app.route('/static/<filename:path>', 'GET',
-                       functools.partial(bottle.static_file, root=STATIC_DIR))
-        self.app.route('/files', 'GET', self.get_file_list)
-        # Stored non-application files
-        self.app.route('/files/get/<filename:path>', 'GET', self.get_file)
-        self.app.route('/files/search', 'GET', self.search)
-        self.app.route('/files/lucky', 'GET', self.lucky)
-        self.app.route('/files/upload', 'POST', self.upload_file)
-        self.app.route('/files/add-from-url', 'POST', self.add_from_url)
+        self.app = sanic.Sanic(__package__)
+        # Static files
+        self.app.static("/static", STATIC_DIR)
+        # Routes
+        self.app.add_route(redirect_home, "/")
+        self.app.add_route(self.get_file_list, "/files")
+        self.app.add_route(self.get_file, "/files/get/<filename:path>")
+        self.app.add_route(self.search, "/files/search")
+        self.app.add_route(self.lucky, "/files/lucky")
+        self.app.add_route(self.upload_file, "/files/upload", methods=["POST"])
+        self.app.add_route(self.add_from_url,
+                           "/files/add-from-url",
+                           methods=["POST"])
+        # Start up task
+        self.app.add_task(self.on_start_up)
 
         self.all_file_names = set()
         self.all_files = []
         self.last_map_time = None
 
-    def build_file_maps(self):
+    async def on_start_up(self, _) -> None:
+        # Build initial map on start-up
+        self.build_file_maps()
+
+    def build_file_maps(self) -> Dict:
         self.all_files = []
         self.all_file_names = set()
         # Local files
         for filename in os.listdir(self.file_store):
             name, full_name = make_sound_name(filename)
-            LOG.debug("Adding local file '%s' to map", name)
+            logger.debug("Adding local file '%s' to map", name)
             self.all_files.append({'name': name, 'full_name': full_name})
             self.all_file_names.add(full_name)
 
@@ -105,7 +115,7 @@ class SoundServer:
                 name, full_name = make_sound_name(entry['url'],
                                                   name=entry['name'])
                 if name in self.all_file_names:
-                    LOG.debug(
+                    logger.debug(
                         "File named '%s' already exists in map, not adding "
                         "to map", name)
                 else:
@@ -115,15 +125,14 @@ class SoundServer:
                         'remote_url': entry['url'],
                     })
                     self.all_file_names.add(full_name)
-        LOG.debug("Added %d sounds to map", len(self.all_files))
+        logger.debug("Added %d sounds to map", len(self.all_files))
 
         # Record when we build this map
         self.last_map_time = datetime.utcnow()
-        LOG.debug('Successfully built map at %s', self.last_map_time)
+        logger.debug('Successfully built map at %s', self.last_map_time)
         return self.all_files
 
-    @property
-    def enriched_file_map(self) -> Dict:
+    def enriched_file_map(self, request_url: str) -> Dict:
         """
         Create a JSON-compatible list of files, with the urls patched to
         the requester's hostname. Eg. if the client requested localhost, patch
@@ -134,14 +143,14 @@ class SoundServer:
         fmap = {'files': self.all_files[:]}
         for i in range(len(fmap['files'])):
             fmap['files'][i]['url'] = utils.replace_url_path(
-                str(bottle.request.url),
+                str(request_url),
                 '/files/get/{}'.format(fmap['files'][i]['full_name']))
         return fmap
 
-    def search(self) -> Dict:
-        search_term = bottle.request.query.search
-        if bottle.request.query.limit:
-            limit = int(bottle.request.query.limit)
+    async def search(self, request: sanic.Request) -> sanic.HTTPResponse:
+        search_term = request.args.get('search')
+        if 'limit' in request.args:
+            limit = int(request.args.get('limit'))
         else:
             limit = DEFAULT_SEARCH_LIMIT
 
@@ -152,84 +161,86 @@ class SoundServer:
         for x in self.all_files:
             if x['full_name'] in (m[0] for m in matches):
                 results['files'].append(x)
-        return results
+        return sanic.response.json(results)
 
-    def lucky(self):
-        search_term = bottle.request.query.search
+    async def lucky(self, request: sanic.Request) -> sanic.HTTPResponse:
+        search_term = request.args.get("search")
         matches = find_closest_matches(search_term,
                                        self.all_file_names,
                                        limit=1)
         if matches:
             for x in self.all_files:
                 if x['full_name'] == matches[0][0]:
-                    LOG.info("Redirecting client to file '%s'", x['full_name'])
-                    raise bottle.HTTPError(303,
-                                           location="/files/{}".format(
-                                               x['full_name']))
-        raise bottle.HTTPError(404, "u r unlucki")
+                    logger.info("Redirecting client to file '%s'",
+                                x['full_name'])
+                    return sanic.response.redirect("/files/get/{}".format(
+                        x['full_name']))
+        raise sanic.exceptions.NotFound("u r unlucki")
 
-    def get_file_list(self):
+    async def get_file_list(self, request: sanic.Request) -> sanic.HTTPResponse:
         # Build a fresh map, if we haven't built it recently
         if self.last_map_time and \
                 datetime.utcnow() - self.last_map_time >= self.MAP_INTERVAL:
             self.build_file_maps()
 
         # Return the enriched copy
-        return self.enriched_file_map
+        enriched = self.enriched_file_map(request.url)
+        return sanic.response.json(enriched)
 
-    def get_data_for_filename(self, filename):
+    def get_data_for_filename(self, filename: str) -> Dict:
         if filename in self.all_file_names:
             for x in self.all_files:
                 if x['full_name'] == filename:
                     return x
         raise FileNotFoundError
 
-    def get_file(self, filename):
+    async def get_file(self, request: sanic.Request,
+                       filename: str) -> sanic.HTTPResponse:
         try:
             data = self.get_data_for_filename(filename)
         except FileNotFoundError:
-            raise bottle.HTTPError(404, "File not found") from None
+            raise sanic.exceptions.FileNotFound(
+                "File not found", filename,
+                "/files/get/{}".format(filename)) from None
 
         if 'remote_url' in data:
             # This is a remote file, so download & save it now
-            LOG.info("'%s' is a non-local file, getting it from remote",
-                     filename)
-            download_remote_file(
+            logger.info("'%s' is a non-local file, getting it from remote",
+                        filename)
+            await download_remote_file(
                 data['remote_url'],
                 os.path.join(self.file_store, data['full_name']))
             # Rebuild the filemap
             self.build_file_maps()
 
-        # Return the file that is guaranteed to exist in the file store
-        LOG.info("Serving local file '%s'", filename)
-        return bottle.static_file(filename, root=self.file_store)
+        # Return the file that is now guaranteed to exist in the file store
+        logger.info("Serving local file '%s'", filename)
+        abs_path = os.path.join(self.file_store, filename)
+        return await sanic.response.file(abs_path)
 
-    def upload_file(self):
-        upload = bottle.request.files['upload']
+    async def upload_file(self, request: sanic.Request) -> sanic.HTTPResponse:
+        upload = request.files['upload']
         check_file_extension(upload)
-        upload.save(self.file_store)  # appends upload.filename automatically
-        return 'ok'
+        save_as = os.path.join(self.file_store, upload.name)
+        async with aiofiles.open(save_as, 'wb') as fp:
+            await fp.write(upload.body)
+        return sanic.response.text('ok')
 
-    def add_from_url(self):
-        ext = check_file_extension(bottle.request.forms["url"])
-        filename = os.path.join(self.file_store,
-                                bottle.request.forms["name"] + ext)
-        with open(filename, 'wb') as fp:
-            response = requests.get(bottle.request.forms["url"])
-            fp.write(response.content)
-        return 'ok'
+    async def add_from_url(self, request: sanic.Request) -> sanic.HTTPResponse:
+        ext = check_file_extension(request.form["url"])
+        save_as = os.path.join(self.file_store, request.form["name"] + ext)
+        await download_remote_file(request.form["url"], save_as)
+
+        return sanic.response.empty()
 
     def run(self, *args, **kwargs):
-        # Build initial map on start-up
-        self.build_file_maps()
         self.app.run(*args, **kwargs)
 
 
 def main():
     args = cli.parse_args()
-    LOG.setLevel(logging.DEBUG if args.dev_mode else logging.INFO)
     sound_server = SoundServer(args.store)
-    sound_server.run(server='wsgiref' if args.dev_mode else 'tornado',
-                     host=args.host,
+    sound_server.run(host=args.host,
                      port=args.port,
-                     reloader=args.dev_mode)
+                     debug=args.dev_mode,
+                     access_log=args.dev_mode)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 from unittest import mock
 import pytest
+import sanic
+import os.path
 from soundserver import main
 
 
@@ -12,3 +14,72 @@ def test_make_sound_name_url():
     short, full = main.make_sound_name(url)
     assert short == 'file'
     assert full == 'file.txt'
+
+
+@pytest.mark.asyncio
+@mock.patch('soundserver.main.aiofiles.open')
+@mock.patch('soundserver.main.httpx.AsyncClient.get',
+            new_callable=mock.AsyncMock)
+async def test_download_remote_file_200(get_mock, open_mock):
+    # Arrange
+    get_mock.return_value = mock.Mock(status_code=200, content=b'123')
+    file_handle_mock = mock.AsyncMock()
+    open_mock.return_value.__aenter__.return_value = file_handle_mock
+    # Act
+    await main.download_remote_file('url', '/path/to/file')
+    # Assert
+    get_mock.assert_awaited_once_with('url')
+    open_mock.assert_called_once_with('/path/to/file', 'wb')
+    file_handle_mock.write.assert_awaited_once_with(b'123')
+
+
+@pytest.mark.asyncio
+@mock.patch('soundserver.main.httpx.AsyncClient.get',
+            new_callable=mock.AsyncMock)
+async def test_download_remote_file_error(get_mock):
+    # Arrange
+    get_mock.return_value = mock.Mock(status_code=404, text='not found')
+    # Act
+    with pytest.raises(sanic.exceptions.ServerError):
+        await main.download_remote_file('url', '/path/to/file')
+    # Assert
+    get_mock.assert_awaited_once_with('url')
+
+
+class TestSoundServer:
+    FILE_STORE = '/path/to/store'
+
+    @pytest.fixture
+    def soundserver(self) -> main.SoundServer:
+        return main.SoundServer(self.FILE_STORE)
+
+    def make_request(self):
+        with mock.patch("sanic.Request.receive_body") as receive_body_mock:
+            request = sanic.Request(
+                b'/test',
+                headers={"Content-Type": "application/x-www-form-urlencoded"},
+                method="POST",
+                transport=mock.Mock(),
+                app=mock.Mock(),
+                version="1.1")
+            body = "url=value1&field2=value2"
+            receive_body_mock.return_value = body
+        return request
+
+    @pytest.mark.asyncio
+    @mock.patch('soundserver.main.download_remote_file')
+    @mock.patch('soundserver.main.check_file_extension')
+    async def test_add_from_url(self, check_mock, download_mock, soundserver):
+        # Arrange
+        check_mock.return_value = '.wav'
+        # request = self.make_request()
+        request = mock.MagicMock(spec=sanic.Request)
+        request.form = {"url": "url_val", "name": "name_val"}
+        # Act
+        result = await soundserver.add_from_url(request)
+        # Assert
+        check_mock.assert_called_once_with("url_val")
+        download_mock.assert_awaited_once_with(
+            "url_val", os.path.join(self.FILE_STORE, "name_val.wav"))
+        assert isinstance(result, sanic.HTTPResponse)
+        assert result.status == 204
